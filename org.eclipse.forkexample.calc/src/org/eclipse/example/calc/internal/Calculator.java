0b6dd9ac8844d6cb475b6f823a30b4ebda5ab534
xgadupd <Dennis.Uphoff@fiduciagad.de>
2021-02-10 11:34:43 +0100

    another one

--- org.eclipse.forkexample.calc/src/org/eclipse/example/calc/internal/Calculator.java
+++ org.eclipse.forkexample.calc/src/org/eclipse/example/calc/internal/Calculator.java
@@ -40,6 +40,7 @@ public class Calculator {
 	    // Das ist der zweite Kommentar in master
 	    // auch noch ein Kommentar von Ruth
 	    //Hier ist ein weiterer Kommentar
+	    //Ist so schön, gibt noch einen weiteren!
 		new Equals();
 		new Minus();
 		new Plus();
