3ae400daa4baacf23c727981e27af4ffde0b05d8
Susanne Rieke <susanne.rieke@fiduciagad.de>
2021-02-10 11:51:33 +0100

    Änderung im Master

--- org.eclipse.forkexample.calc/src/org/eclipse/example/calc/BinaryOperation.java
+++ org.eclipse.forkexample.calc/src/org/eclipse/example/calc/BinaryOperation.java
@@ -19,4 +19,5 @@ public interface BinaryOperation extends Operation {
 	 * @return result of binary operation 
 	 */ 
 	public float perform(float arg1, float arg2);
+	 // ein Kommentar von xgadsra
 }
